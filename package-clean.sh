b52c374f8d3360589ff79fec28d59865fe63a9da
Tsuyoshi Inamori <inamori@getelementby.id>
2022-02-07 21:55:20 +0900

    掃除スクリプト

---
+++ package-clean.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -rfv \
+  template/node_modules\
+  template/public/wp/wp-admin\
+  template/public/wp/wp-content\
+  template/public/wp/wp-includes\
+  template/public/wp/index.php\
+  template/public/wp/license.txt\
+  template/public/wp/readme.html\
+  template/public/wp/wp-*.php\
+  template/public/wp/xmlrpc.php\
+  template/vendor\
+  template/package-lock.json
